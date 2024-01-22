5c59b748317c014c9b953e98319aa5a47b864fc6
shoi <138567675+shoooi0801@users.noreply.github.com>
2024-01-22 08:46:15 +0000

    migrateまで

--- db/migrate/20240122081621_create_posts.rb
+++ db/migrate/20240122081621_create_posts.rb
@@ -1,7 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-
+      t.integer :user_id, null: false
+      t.string :name, null: false
+      t.string :parking, null: false
+      t.string :postal_code, null: false
+      t.string :address, null: false
+      t.text :introduction, null: false
       t.timestamps
     end
   end
