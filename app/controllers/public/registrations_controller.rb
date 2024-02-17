946dbd02cd7d88240631f790471c68f41ea4169d
shoi <138567675+shoooi0801@users.noreply.github.com>
2024-02-17 09:14:36 +0000

    サインアップストロングパラメーター

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
+  def after_update_path_for(resource)
+    root_path
+  end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :introduction, :is_active])
+  end
 
   # GET /resource/sign_up
   # def new
