ad46a44ec8eb06a4803368627d759a73dd5a4432
shoi <138567675+shoooi0801@users.noreply.github.com>
2024-02-21 11:12:36 +0000

    ログイン画面等

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,3 @@
 class Public::PostsController < ApplicationController
+  before_action :authenticate_customer!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,9 @@ devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
 
+  scope module: :public do
+    root 'homes#top'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
