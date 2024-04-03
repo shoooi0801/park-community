6af3a7ec7fde2003cc3fab3146a5e1204c7e6474
shoi <138567675+shoooi0801@users.noreply.github.com>
2024-04-03 06:19:12 +0000

    mypage更新

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -8,4 +8,15 @@ class Public::CustomersController < ApplicationController
     @customer = current_customer
   end
 
+  def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to show_customers_path
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:name, :email, :introduction)
+  end
+
 end
